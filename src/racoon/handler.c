db32e213ee9b35a312f97f4b52b6548c3423bb2a
vanhu <vanhu>
2007-03-23 15:34:31 +0000

    expire zombie handlers in getph2byid(), to avoid situations where we'll never negociate a phase2 again

--- src/racoon/handler.c
+++ src/racoon/handler.c
@@ -480,8 +480,21 @@ getph2byid(src, dst, spid)
 	LIST_FOREACH(p, &ph2tree, chain) {
 		if (spid == p->spid &&
 		    CMPSADDR(src, p->src) == 0 &&
-		    CMPSADDR(dst, p->dst) == 0)
-			return p;
+		    CMPSADDR(dst, p->dst) == 0){
+			/* Sanity check to detect zombie handlers
+			 * XXX Sould be done "somewhere" more interesting,
+			 * because we have lots of getph2byxxxx(), but this one
+			 * is called by pk_recvacquire(), so is the most important.
+			 */
+			if(p->status < PHASE2ST_ESTABLISHED &&
+			   p->retry_counter == 0
+			   && p->sce == NULL && p->scr == NULL){
+				plog(LLV_DEBUG, LOCATION, NULL,
+					 "Zombie ph2 found, expiring it\n");
+				isakmp_ph2expire(p);
+			}else
+				return p;
+		}
 	}
 
 	return NULL;
