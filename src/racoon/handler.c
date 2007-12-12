8f0578de0b0e6df8a6d4676a9e8f65bed1fcda86
mgrooms <mgrooms>
2007-12-12 04:45:59 +0000

    Add support for nat-t oa payload handling. Submitted by Timo Teras.

--- src/racoon/handler.c
+++ src/racoon/handler.c
@@ -656,13 +656,23 @@ delph2(iph2)
 		iph2->dst = NULL;
 	}
 	if (iph2->src_id) {
-	      racoon_free(iph2->src_id);
-	      iph2->src_id = NULL;
+		racoon_free(iph2->src_id);
+		iph2->src_id = NULL;
 	}
 	if (iph2->dst_id) {
-	      racoon_free(iph2->dst_id);
-	      iph2->dst_id = NULL;
+		racoon_free(iph2->dst_id);
+		iph2->dst_id = NULL;
 	}
+#ifdef ENABLE_NATT
+	if (iph2->natoa_src) {
+		racoon_free(iph2->natoa_src);
+		iph2->natoa_src = NULL;
+	}
+	if (iph2->natoa_dst) {
+		racoon_free(iph2->natoa_dst);
+		iph2->natoa_dst = NULL;
+	}
+#endif
 
 	if (iph2->proposal) {
 		flushsaprop(iph2->proposal);
