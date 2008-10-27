aa1a57691e00593740b0f6efca7340f2e5762374
tteras <tteras>
2008-10-27 06:21:29 +0000

    From Arnaud Ebalard: Fix indentation issues for readability

--- src/racoon/grabmyaddr.c
+++ src/racoon/grabmyaddr.c
@@ -403,10 +403,10 @@ grab_myaddrs()
 #endif
 #endif
 		if (getnameinfo(p->addr, sysdep_sa_len(p->addr),
-				addr1, sizeof(addr1),
-				NULL, 0,
+				addr1, sizeof(addr1), NULL, 0,
 				NI_NUMERICHOST | niflags))
-		strlcpy(addr1, "(invalid)", sizeof(addr1));
+			strlcpy(addr1, "(invalid)", sizeof(addr1));
+
 		plog(LLV_DEBUG, LOCATION, NULL,
 			"my interface: %s (%s)\n",
 			addr1, ifap->ifa_name);
@@ -519,10 +519,10 @@ grab_myaddrs()
 #endif
 #endif
 			if (getnameinfo(p->addr, sysdep_sa_len(p->addr),
-					addr1, sizeof(addr1),
-					NULL, 0,
+					addr1, sizeof(addr1), NULL, 0,
 					NI_NUMERICHOST | niflags))
-			strlcpy(addr1, "(invalid)", sizeof(addr1));
+				strlcpy(addr1, "(invalid)", sizeof(addr1));
+
 			plog(LLV_DEBUG, LOCATION, NULL,
 				"my interface: %s (%s)\n",
 				addr1, ifr->ifr_name);
